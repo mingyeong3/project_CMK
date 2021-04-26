c1962eab24cdfef39854392378893174d3c4c772
mingyeong3 <83192186+mingyeong3@users.noreply.github.com>
2021-04-26 17:37:15 +0900

    자바1일차 연습문제

---
+++ JAVA_CMK/src/day1/test1_3.java
@@ -0,0 +1,38 @@
+package day1;
+
+import java.util.Scanner;
+
+public class test1_3 {
+
+	public static void main(String[] args) {
+		// 두 정수와 문자를 입력받아 출력하는 예제를 작성하세요.
+	    /* 정수1을 입력하세요 : 1
+	     * 문자를 입력하세요 : +
+	     * 정수2를 입력하세요 : 2
+	     * 1 + 2
+	     * */
+
+		 Scanner scan = new Scanner(System.in);
+		 
+		 System.out.print("정수1을 일렵하세요 : ");
+		 int num1 = scan.nextInt();
+		 System.out.println("문자를 입력하세요 : ");
+		 char ch = scan.next().charAt(0);
+		 System.out.println("정수2를 입력하세요 : ");
+		 int num2 = scan.nextInt();
+		 System.out.println(""+ num1 + ch + num2);
+		 
+		 
+		 System.out.println("두 정수와 문자를 입력하세요 (예: 1 + 2) :");
+		 num1 = scan.nextInt();
+		 ch = scan.next().charAt(0);
+		 num2 = scan.nextInt();
+		 System.out.println("+ num1 + ch + num2");
+		 
+				 
+		 
+	
+	}
+
+	
+}
